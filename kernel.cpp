fb2422057f96551104e727f609ce113dcfca2086
Iregon <0iregon0@gmail.com>
2019-04-29 14:35:55 +0200

    Refactoring

--- kernel.cpp
+++ kernel.cpp
@@ -42,19 +42,14 @@ typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 extern "C" void callConstructors() {
-    for(constructor* i = &start_ctors; i != &end_ctors; i++) {
-        printf("ciao");
-        printf((char*)i);
-        printf("\n");
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
         (*i)();
-    }
 }
 
 
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/) {
     printf("Hello World! --- Alessandro Tornesello --- https://github.com/iregon/my_os\n");
-    
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
     
