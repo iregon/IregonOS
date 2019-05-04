8746ec9ff624ac8adba90ad3dce3095553210516
iregon <0iregon0@gmail.com>
2019-05-04 19:23:19 +0200

    Cleaning

--- include/gui/widget.h
+++ include/gui/widget.h
@@ -9,98 +9,98 @@ namespace iregonos {
     namespace gui {
         class Widget {
         protected:
-            Widget* parent;
+            Widget *parent;
             common::int32_t x;
-            
+
             common::int32_t y;
-            
+
             common::int32_t w;
-            
+
             common::int32_t h;
 
             common::uint8_t r;
-            
+
             common::uint8_t g;
-            
+
             common::uint8_t b;
-            
+
             bool Focussable;
 
-         public:
+        public:
 
-             Widget(Widget* parent, 
-                    common::int32_t x, 
-                    common::int32_t y, 
-                    common::int32_t w, 
-                    common::int32_t h,
-                    common::uint8_t r, 
-                    common::uint8_t g, 
-                    common::uint8_t b);
-             
+            Widget(Widget *parent,
+                   common::int32_t x,
+                   common::int32_t y,
+                   common::int32_t w,
+                   common::int32_t h,
+                   common::uint8_t r,
+                   common::uint8_t g,
+                   common::uint8_t b);
+
             ~Widget();
 
-            virtual void GetFocus(Widget* widget);
-            
-            virtual void ModelToScreen(common::int32_t &x, 
-                                       common::int32_t& y);
+            virtual void GetFocus(Widget *widget);
 
-            virtual void Draw(GraphicsContext* gc);
-            
-            virtual void OnMouseDown(common::int32_t x, 
+            virtual void ModelToScreen(common::int32_t &x,
+                                       common::int32_t &y);
+
+            virtual void Draw(GraphicsContext *gc);
+
+            virtual void OnMouseDown(common::int32_t x,
                                      common::int32_t y);
-            
-            virtual void OnMouseUp(common::int32_t x, 
+
+            virtual void OnMouseUp(common::int32_t x,
                                    common::int32_t y);
-            
-            virtual void OnMouseMove(common::int32_t oldx, 
-                                     common::int32_t oldy, 
-                                     common::int32_t newx, 
+
+            virtual void OnMouseMove(common::int32_t oldx,
+                                     common::int32_t oldy,
+                                     common::int32_t newx,
                                      common::int32_t newy);
 
-            virtual void OnKeyDown(common::int32_t x, 
+            virtual void OnKeyDown(common::int32_t x,
                                    common::int32_t y);
-            
-            virtual void OnKeyUp(common::int32_t x, 
+
+            virtual void OnKeyUp(common::int32_t x,
                                  common::int32_t y);
         };
 
         class CompositeWidget : public Widget {
         private:
-            Widget* children[100];
+            Widget *children[100];
             int numChildren;
-            Widget* focussedChild;
+            Widget *focussedChild;
 
-         public:
-            CompositeWidget(Widget* parent,
-                            common::int32_t x, 
-                            common::int32_t y, 
-                            common::int32_t w, 
+        public:
+            CompositeWidget(Widget *parent,
+                            common::int32_t x,
+                            common::int32_t y,
+                            common::int32_t w,
                             common::int32_t h,
-                            common::uint8_t r, 
-                            common::uint8_t g, 
+                            common::uint8_t r,
+                            common::uint8_t g,
                             common::uint8_t b);
-            
-            ~CompositeWidget();            
 
-            virtual void GetFocus(Widget* widget);
+            ~CompositeWidget();
 
-            virtual void Draw(GraphicsContext* gc);
-            
-            virtual void OnMouseDown(common::int32_t x, 
+            virtual void GetFocus(Widget *widget);
+
+            virtual void Draw(GraphicsContext *gc);
+
+            virtual void OnMouseDown(common::int32_t x,
                                      common::int32_t y);
-            
-            virtual void OnMouseUp(common::int32_t x, 
+
+            virtual void OnMouseUp(common::int32_t x,
                                    common::int32_t y);
-            
-            virtual void OnMouseMove(common::int32_t oldx, 
-                                     common::int32_t oldy, 
-                                     common::int32_t newx, 
+
+            virtual void OnMouseMove(common::int32_t oldx,
+                                     common::int32_t oldy,
+                                     common::int32_t newx,
                                      common::int32_t newy);
 
-            virtual void OnKeyDown(common::int32_t x, 
+            virtual void OnKeyDown(common::int32_t x,
                                    common::int32_t y);
-            
-            virtual void OnKeyUp(common::int32_t x, 
+
+            virtual void OnKeyUp(common::int32_t x,
                                  common::int32_t y);
         };
     }
